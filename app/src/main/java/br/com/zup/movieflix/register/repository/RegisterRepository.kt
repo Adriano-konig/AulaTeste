5338739049f15efda874920dbd770f30e80ff228
Adriano-konig <konigadri87@hotmail.comgit config --global user.email konigadri87@hotmail.com>
2022-07-20 17:11:58 -0300

    teste Register

---
+++ app/src/main/java/br/com/zup/movieflix/register/repository/RegisterRepository.kt
@@ -0,0 +1,17 @@
+package br.com.zup.movieflix.register.repository
+
+
+import br.com.zup.movieflix.register.model.RegisterModel
+
+class RegisterRepository {
+    fun authenticate (register: RegisterModel) : RegisterModel {
+
+        if(register.senha == register.confirmaSenha){
+            register.acessAuth = true
+        }else{
+            register.acessAuth = false
+        }
+        return register
+    }
+
+}
